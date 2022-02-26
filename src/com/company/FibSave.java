8db0d08f4d5b6a4a446a85ce79804ac0a131bef1
Alexey Gorshkov <alexmilesjg@gmail.com>
2022-02-27 01:12:49 +0500

    corrected

--- src/com/company/FibSave.java
+++ src/com/company/FibSave.java
@@ -7,18 +7,18 @@ public class FibSave {
 
     public static List<Integer> fibMass= new ArrayList(); // создаем массив
 
-    // Добавляем 0 и 1 элемент последовательности
-    static {
-        fibMass.add(0);
-        fibMass.add(1);
-    }
-
     public static int fibSave(int n) {
+
         int i;
 
+        if( fibMass.size()==0 ) {   // Добавляем 0 и 1 элемент последовательности
+            fibMass.add(0);
+            fibMass.add(1);
+        }
         for (i = fibMass.size(); i <= n; i++) {
-                fibMass.add(fibMass.get(i - 1) + fibMass.get(i - 2));
-            }
+            fibMass.add(fibMass.get(i - 1) + fibMass.get(i - 2));
+        }
+
         return fibMass.get(n);
 
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(FibRecursion.fibRec(11));
+        System.out.println(FibRecursion.fibRec(5));
 
-        System.out.println(FibSimple.fibSimp(11));
+        System.out.println(FibSimple.fibSimp(6));
 
-        System.out.println(FibSave.fibSave(11));
+        System.out.println(FibSave.fibSave(7));
 
     }
 }
